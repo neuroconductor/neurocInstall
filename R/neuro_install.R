c21ffd849601e661fcf9806aeec215fe5af6dbbc
muschellij2 <muschellij2@gmail.com>
2017-01-23 17:41:39 -0500

    Neuro_install script

--- R/neuro_install.R
+++ R/neuro_install.R
@@ -16,43 +16,6 @@ neuro_install = function(repo,
                          upgrade_dependencies = TRUE,
                          ...){
 
-  #########################################
-  # Checking devtools version
-  #########################################
-  get_devtools_version = function() {
-    ipacks = installed.packages()
-    dtools = ipacks[ ipacks[, "Package"] %in% "devtools", "Version"]
-    return(dtools)
-  }
-  # needed for bioc stuff
-  req_version = "1.12.0.9000"
-  dtools = get_devtools_version()
-  install_from_cran = FALSE
-  if (length(dtools) == 0 ) {
-    install_from_cran = TRUE
-  } else {
-    # too low of a version
-    install_from_cran = compareVersion(dtools, req_version) < 0
-  }
-  if (install_from_cran) {
-    install.packages("devtools")
-  }
-
-  # now we assume devtools is installed
-  dtools = get_devtools_version()
-  if (length(dtools) == 0 ) {
-    stop(paste0("devtools tried to install but could not ",
-                "- try to install devtools manually.",
-                "Version >= ", req_version, " required."
-                )
-    )
-  } else {
-    comparison = compareVersion(dtools, "1.12.0.9000")
-    if (comparison < 0) {
-      devtools::install_github("hadley/devtools")
-    }
-  }
-
   #############################
   # Create a data.frame for merging
   #############################
