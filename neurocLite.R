d2845cf0879a131226c282967434540388a5f7b0
muschellij2 <muschellij2@gmail.com>
2017-01-23 17:43:10 -0500

    updating top

--- neurocLite.R
+++ neurocLite.R
@@ -1,10 +1,43 @@
-# neurocInstall package version: 0.4.1
-pkg_ver = '# neurocInstall package version: 0.4.1'
-if (!require("neurocInstall")) {
-  if (!require(devtools)) {
-    install.packages("devtools")
+# neurocInstall package version: 0.4.2
+pkg_ver = '# neurocInstall package version: 0.4.2'
+# if (!require("neurocInstall")) {
+#########################################
+# Checking devtools version
+#########################################
+get_devtools_version = function() {
+  ipacks = installed.packages()
+  dtools = ipacks[ ipacks[, "Package"] %in% "devtools", "Version"]
+  return(dtools)
+}
+# needed for bioc stuff
+req_version = "1.12.0.9000"
+dtools = get_devtools_version()
+install_from_cran = FALSE
+if (length(dtools) == 0 ) {
+  install_from_cran = TRUE
+} else {
+  # too low of a version
+  install_from_cran = compareVersion(dtools, req_version) < 0
+}
+if (install_from_cran) {
+  install.packages("devtools")
+}
+
+# now we assume devtools is installed
+dtools = get_devtools_version()
+if (length(dtools) == 0 ) {
+  stop(paste0("devtools tried to install but could not ",
+              "- try to install devtools manually.",
+              "Version >= ", req_version, " required."
+  )
+  )
+} else {
+  comparison = compareVersion(dtools, "1.12.0.9000")
+  if (comparison < 0) {
+    devtools::install_github("hadley/devtools")
   }
-  message(paste("Using neurocLite version:", pkg_ver))
+}
+message(paste("Using neurocLite version:", pkg_ver))
 	#' @title Neuroconductor Installer
 	#' @description Install function for neuroconductor packages
 	#' @param repo Package name in neuroconductor
@@ -22,43 +55,6 @@ if (!require("neurocInstall")) {
 	                         release = c("stable", "development"),
 	                         upgrade_dependencies = TRUE,
 	                         ...){
-	
-	  #########################################
-	  # Checking devtools version
-	  #########################################
-	  get_devtools_version = function() {
-	    ipacks = installed.packages()
-	    dtools = ipacks[ ipacks[, "Package"] %in% "devtools", "Version"]
-	    return(dtools)
-	  }
-	  # needed for bioc stuff
-	  req_version = "1.12.0.9000"
-	  dtools = get_devtools_version()
-	  install_from_cran = FALSE
-	  if (length(dtools) == 0 ) {
-	    install_from_cran = TRUE
-	  } else {
-	    # too low of a version
-	    install_from_cran = compareVersion(dtools, req_version) < 0
-	  }
-	  if (install_from_cran) {
-	    install.packages("devtools")
-	  }
-	
-	  # now we assume devtools is installed
-	  dtools = get_devtools_version()
-	  if (length(dtools) == 0 ) {
-	    stop(paste0("devtools tried to install but could not ",
-	                "- try to install devtools manually.",
-	                "Version >= ", req_version, " required."
-	                )
-	    )
-	  } else {
-	    comparison = compareVersion(dtools, "1.12.0.9000")
-	    if (comparison < 0) {
-	      devtools::install_github("hadley/devtools")
-	    }
-	  }
 	
 	  #############################
 	  # Create a data.frame for merging
