bde1b53a4c00f051db1adf79de564b31cf938f47
muschellij2 <muschellij2@gmail.com>
2018-02-01 13:39:32 -0500

    Added make_release_version.

--- R/latest_neuroc_release.R
+++ R/latest_neuroc_release.R
@@ -12,8 +12,16 @@ latest_neuroc_release = function(
   secure = TRUE) {
   # release_version = "2017/nov/"
   release = match.arg(release)
-  release_version = paste0("latest/", release, "/")
-    release_version = paste0(
-   "http", ifelse(secure, "s", ""), "://neuroconductor.org/releases/",
+  release_path = paste0("latest/", release, "/")
+  release_version = make_release_version(release_path, secure = secure)
+  return(release_version)
+}
+
+#' @export
+#' @param release_path path to the release on
+#' \url{https://neuroconductor.org/releases/}
+make_release_version = function(release_path, secure = TRUE) {
+  release_version = paste0(
+    "http", ifelse(secure, "s", ""), "://neuroconductor.org/releases/",
     release_version)
 }
