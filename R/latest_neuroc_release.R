903aff5c1f7be7e3f366314321c3c2921ccbd077
muschellij2 <muschellij2@gmail.com>
2018-02-01 13:43:26 -0500

    FIxing latest_neuroc_release.R

--- R/latest_neuroc_release.R
+++ R/latest_neuroc_release.R
@@ -17,9 +17,10 @@ latest_neuroc_release = function(
   return(release_version)
 }
 
-#' @export
+#' @rdname latest_neuroc_release
 #' @param release_path path to the release on
 #' \url{https://neuroconductor.org/releases/}
+#' @export
 make_release_version = function(release_path, secure = TRUE) {
   release_path = paste0(
     "http", ifelse(secure, "s", ""), "://neuroconductor.org/releases/",
